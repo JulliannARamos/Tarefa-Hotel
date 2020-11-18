9d10cbc20cf34d9036a194d56b4f252e4264c375
JulliannARamos <69683252+JulliannARamos@users.noreply.github.com>
2020-11-18 17:24:47 -0300

    Add files via upload

---
+++ Hotel18/Hotel18/Hospedes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel18
+{
+    class Hospedes
+    {
+
+        string nome;
+        string email;
+
+        public string Nome { get => nome; set => nome = value; }
+        public string Email { get => email; set => email = value; }
+
+    }
+}
